5be7b697d5f5e9762def7b2e32b7ef877bec86de
LeoDomGit <leodomsolar@gmail.com>
2023-11-29 17:09:07 +0700

    C

---
+++ src/session3/404/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Icon from "./Icon";
+function NotFound() {
+  return (
+    <>
+      <div style={{ width: "30%", height: "auto", margin: "0px auto" }}>
+        <Icon />
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
